b09efd12e4800342d52541b1975c4ddad3a122ec
torhovenkov <vladislav.lnx@gmail.com>
2023-06-05 14:01:24 +0300

    Subtitle style works

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,15 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Vladyslav Torhovenkov on 04.06.2023.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
